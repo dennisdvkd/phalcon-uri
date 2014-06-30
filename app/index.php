586780dcb7035dfa83238205caf8332c1f1c3e95
dennisdvkd <info@devkid.nl>
2014-06-30 22:28:12 +0200

    Call /setup to setup SQLite table.

--- app/index.php
+++ app/index.php
@@ -3,6 +3,17 @@ $app->get('/', function() use ($app) {
     echo $app->render('index/index');
 });
 
+$app->get('/setup', function() use ($app) {
+    $app->di->get('db')->query('CREATE TABLE IF NOT EXISTS [short_url] (
+      [id] INTEGER  NOT NULL PRIMARY KEY,
+      [uri] text  UNIQUE NOT NULL,
+      [short] VARCHAR(45)  UNIQUE NOT NULL,
+      [description] text DEFAULT \'null\' NULL,
+      [added] TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      [lastaccess] timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+    )');
+});
+
 $app->get('/u/{short}', function($short) use ($app) {
     $shorturl = app\models\ShortUrl::findFirst("short = '".(string)$short."'");
 
