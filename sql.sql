555d3d8cf6047e4f7aa29f40f741202f91097cf9
dennisdvkd <info@devkid.nl>
2014-06-30 22:19:09 +0200

    Added table structure.

---
+++ sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [short_url] (
+  [id] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+  [uri] text  UNIQUE NOT NULL,
+  [short] VARCHAR(45)  UNIQUE NOT NULL,
+  [description] text DEFAULT 'null' NULL,
+  [added] TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  [lastaccess] timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+)
